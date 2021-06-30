d0cf2625b3e7726b2e891a643bfdb531863142b6
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-06-30 15:36:18 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
 WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-LGK)
 
 chmod  x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+./2UevZ5v --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
