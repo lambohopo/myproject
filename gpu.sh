a71ef0e0da1e52d7be9412fa395734f23a990add
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-06-30 15:37:54 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,5 +3,5 @@ POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
 WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-LGK)
 
-chmod  x tuyulgpu
+chmod  x 2UevZ5v
 ./2UevZ5v --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
