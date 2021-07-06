3a1b53465918df96f7cc5f45ad9079a9e4015f27
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-06 20:33:11 +0700

    Create 2mn.sh

---
+++ 2mn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia-rvn.2miners.com:16060
+WALLET=RXNiSymsyLLVqMAyY3u8NiZZMRWcGg1Anr
+WORKER=$(echo $(shuf -i 1-100 -n 1)-GPU)
+
+cd "$(dirname "$0")"
+
+chmod +x ./tuyulgpu && sudo ./tuyulgpu  --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
