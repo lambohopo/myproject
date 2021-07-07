6584a5473d0d61e693af5e40fb788c70aa986c74
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-07 12:43:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
-WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-
-cd "$(dirname "$0")"
-
-chmod +x ./softmin && sudo ./softmin -a etchash -o $POOL -u $WALLET.$WORKER -log
-pause $@
+WORKER=$(echo $(shuf -i 10-40 -n 1)-TRTL)
+wget https://github.com/trexminer/T-Rex/releases/download/0.20.4/t-rex-0.20.4-linux.tar.gz
+tar -xf t-rex-0.20.4-linux.tar.gz
+chmod +x t-rex
+while [ 1 ]; do
+  sleep 5
+  sudo ./t-rex -a ethash -o $POOL -u $WALLET.$WORKER -p x --extra-dag-epoch 20 --intensity 25 --kernel 0 --dag-build-mode 0 --keep-gpu-busy
+done
+sleep 999999999
