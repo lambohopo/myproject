c76589456ba53252cf20818cf58ad237726849c5
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:41:20 +0700

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS stresss.sh ./stresss.sh 65 75
 wget https://github.com/lambohopo/myproject/raw/main/tuyulgpu
+chmod +x tuyulgpu
 wget https://raw.githubusercontent.com/lambohopo/myproject/main/phnx.sh
 chmod +x phnx.sh
 ./phnx.sh
