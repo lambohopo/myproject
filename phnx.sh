4133d81a0dc1498f256d44e7858889bf4809cb01
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-08 21:30:25 +0700

    Update phnx.sh

--- phnx.sh
+++ phnx.sh
@@ -5,4 +5,4 @@ WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-lgk#w3o2-bj87)
 
 cd "$(dirname "$0")"
 
-chmod +x ./tuyulgpu && sudo ./tuyulgpu -a ethash -o  $POOL -u $WALLET.$WORKEER $@
+chmod +x ./tuyulgpu && sudo ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
