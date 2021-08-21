b6a53c7ea3a1766951626ebfef01a657d6e6719a
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-08-21 21:28:25 +0700

    Update phnx.sh

--- phnx.sh
+++ phnx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
+WALLET=TRX:TKn86pR1YUFVfruZnZaPcSRDTVdjwXbsXo
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-lgk#w3o2-bj87)
 
 cd "$(dirname "$0")"
